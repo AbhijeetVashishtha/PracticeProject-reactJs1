2f9cbcc25ef974f990d0f2a82a4cc68f1789a915
AbhijeetVashishtha <abhijeetvashishth13>
2023-03-08 23:33:51 +0530

    Working with Portals

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+import Wrappers from '../Helpers/Wrappers';
 
 
 const AddUser = (props) => {
@@ -47,7 +48,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
+        <Wrappers>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
@@ -58,7 +59,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrappers>
     );
 };
 
